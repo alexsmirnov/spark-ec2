1477fe62081eaec168d2a724c2f0a184b80ddf62
Alexander Smirnov <alexander.smirnov@mulesoft.com>
2017-02-10 10:49:30 -0800

    install angular interpreter by default

--- zeppelin/init.sh
+++ zeppelin/init.sh
@@ -6,7 +6,6 @@ zeppelin_ver=0.7.0
 ZEPPELIN_HOME=/root/zeppelin
 ZEPPELIN_CONF_DIR="${ZEPPELIN_HOME}/conf"
 
-MASTER "spark://spark-master:7077"
 ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 ZEPPELIN_MEM=-Xmx1024m
 
@@ -15,6 +14,6 @@ curl http://mirrors.ibiblio.org/apache/zeppelin/zeppelin-${zeppelin_ver}/zeppeli
 mv "/root/zeppelin-${zeppelin_ver}-bin-netinst" "${ZEPPELIN_HOME}"
 ${ZEPPELIN_HOME}/bin/install-interpreter.sh --name md,python,shell,file,alluxio
 # additional interpreters
-${ZEPPELIN_HOME}/bin/install-interpreter.sh --name jdbc,cassandra
+${ZEPPELIN_HOME}/bin/install-interpreter.sh --name jdbc,cassandra,angular
 
 popd > /dev/null
